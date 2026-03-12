0dd60550d17c80b262d07bab1805f2603724d36b
Arpan Raval <arpanraval1112@gmail.com>
2026-03-12 22:52:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from calculator import add, subtract, multiply, divide
+
+print("Calculator Example")
+
+print("Add:", add(5, 3))
+print("Subtract:", subtract(10, 4))
+print("Multiply:", multiply(6, 2))
+print("Divide:", divide(8, 2))
