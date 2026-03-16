344a26b64f95675badcdd106ebd1cef1bfbb6c2f
Arpan Raval <arpanraval1112@gmail.com>
2026-03-17 01:54:05 +0530

    added fast API

--- src/api.py
+++ src/api.py
@@ -3,21 +3,26 @@ from src.calculator import add, subtract, multiply, divide
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Calculator API is running"}
+
 
 @app.get("/add")
 def add_numbers(a: int, b: int):
     return {"result": add(a, b)}
 
+
 @app.get("/subtract")
 def subtract_numbers(a: int, b: int):
     return {"result": subtract(a, b)}
 
+
 @app.get("/multiply")
 def multiply_numbers(a: int, b: int):
     return {"result": multiply(a, b)}
+
 
 @app.get("/divide")
 def divide_numbers(a: int, b: int):
